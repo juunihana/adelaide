b6759640746993c6701a89228c1a33902abef90a
juunihana <juunigatsunohana@gmail.com>
2023-05-02 18:34:05 +0300

    started implementing JWT authentication

--- adelaide_api/src/main/java/dev/juunihana/adelaide/adelaide_api/configuration/SecurityConfig.java
+++ adelaide_api/src/main/java/dev/juunihana/adelaide/adelaide_api/configuration/SecurityConfig.java
@@ -1,5 +1,9 @@
 package dev.juunihana.adelaide.adelaide_api.configuration;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
 
 }
