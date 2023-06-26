0b0fbea34278b2798a548ad048e55aa845485c0d
juunihana <juunigatsunohana@gmail.com>
2023-06-26 17:21:22 +0300

    added posting microservice

---
+++ adelaide_posts/src/main/kotlin/dev/juunihana/adelaide/adelaide_posts/AdelaidePostsApplication.kt
@@ -0,0 +1,11 @@
+package dev.juunihana.adelaide.adelaide_posts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdelaidePostsApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdelaidePostsApplication>(*args)
+}
