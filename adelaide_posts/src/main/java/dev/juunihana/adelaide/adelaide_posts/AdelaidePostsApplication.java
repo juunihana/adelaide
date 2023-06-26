46a4de806c13baa1c92ff6179b50aa768c6ff0e4
juunihana <juunigatsunohana@gmail.com>
2023-06-26 17:35:04 +0300

    added posting microservice

---
+++ adelaide_posts/src/main/java/dev/juunihana/adelaide/adelaide_posts/AdelaidePostsApplication.java
@@ -0,0 +1,13 @@
+package dev.juunihana.adelaide.adelaide_posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdelaidePostsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdelaidePostsApplication.class, args);
+  }
+
+}
---
+++ adelaide_posts/src/test/java/dev/juunihana/adelaide/adelaide_posts/AdelaidePostsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.juunihana.adelaide.adelaide_posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdelaidePostsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
