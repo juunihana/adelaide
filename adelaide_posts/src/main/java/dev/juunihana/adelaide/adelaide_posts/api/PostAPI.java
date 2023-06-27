de57c26f0b71c27b00dcb964df43793ede139cd0
juunihana <juunigatsunohana@gmail.com>
2023-06-27 13:00:52 +0300

    added posting microservice

--- adelaide_posts/src/main/java/dev/juunihana/adelaide/adelaide_posts/api/PostAPI.java
+++ adelaide_posts/src/main/java/dev/juunihana/adelaide/adelaide_posts/api/PostAPI.java
@@ -1,5 +1,30 @@
 package dev.juunihana.adelaide.adelaide_posts.api;
 
+import dev.juunihana.adelaide.adelaide_posts.dto.CreatePostDTO;
+import dev.juunihana.adelaide.adelaide_posts.dto.PostDTO;
+import java.util.List;
+import java.util.Map;
+
 public interface PostAPI {
 
+  PostDTO findById(String id);
+
+  /**
+   * Search posts
+   * query params passed as GET parameters
+   * on frontend these parameters are controlled by vue-router
+   * it means when we are routed to some page there are GET params
+   * and they are passed to search query from post list component
+   */
+  List<PostDTO> search(Map<String, String> requestParams);
+
+  //201
+  //All posts are either have null in their replyTo field or are replies to other posts
+  void create(CreatePostDTO dto);
+
+  //200
+  void update(String id, CreatePostDTO dto);
+
+  //204
+  void delete(String id);
 }
---
+++ adelaide_posts/src/main/java/dev/juunihana/adelaide/adelaide_posts/dto/PostDTO.java
@@ -0,0 +1,5 @@
+package dev.juunihana.adelaide.adelaide_posts.dto;
+
+public class PostDTO {
+
+}
